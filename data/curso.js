655220488b54afdea7ebd140f59be18f9162bd2a
Drworm82 <worm_jim@hotmail.com>
2025-08-08 17:40:37 -0600

    Create curso.js

---
+++ data/curso.js
@@ -0,0 +1,11 @@
+export const modulos = [
+  {
+    id: 1,
+    titulo: "Fundamentos suaves",
+    descripcion: "Introducción a masas batidas ligeras y técnicas básicas.",
+    lecciones: [
+      { titulo: "Qué es el gluten", slug: "que-es-el-gluten" },
+      { titulo: "Bizcocho Genovés", slug: "bizcocho-genoves" }
+    ]
+  }
+];
