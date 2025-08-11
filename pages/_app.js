f9bad029787872665888a6e04e80baadbc9af8ba
Drworm82 <worm_jim@hotmail.com>
2025-08-11 09:07:51 -0600

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import { createPagesBrowserClient } from '@supabase/auth-helpers-nextjs';
 import { SessionContextProvider } from '@supabase/auth-helpers-react';
 import { useState } from 'react';
-import Layout from './components/Layout';
+import Layout from '../components/Layout';
 import '../styles/globals.css';
 
 export default function App({ Component, pageProps }) {
