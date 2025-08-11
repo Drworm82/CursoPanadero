79453b4ba4fa2e9496a033f7cf89f41401c6737a
Drworm82 <worm_jim@hotmail.com>
2025-08-11 11:29:24 -0600

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs';
+import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs/dist/browser';
 import { SessionContextProvider } from '@supabase/auth-helpers-react';
 import Layout from '../components/Layout';
 import '../styles/globals.css';
