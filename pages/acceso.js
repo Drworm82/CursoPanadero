443f814090f0a7a5f37d7048a4e8928562285836
Drworm82 <worm_jim@hotmail.com>
2025-08-11 09:18:45 -0600

    Update acceso.js

--- pages/acceso.js
+++ pages/acceso.js
@@ -1,12 +1,9 @@
-import { createPagesBrowserClient } from '@supabase/auth-helpers-nextjs';
 import { Auth } from '@supabase/auth-ui-react';
 import { ThemeSupa } from '@supabase/auth-ui-shared';
 import { useEffect } from 'react';
 import { useRouter } from 'next/router';
 import { useUser } from '@supabase/auth-helpers-react';
 import Layout from '../components/Layout';
-
-const supabase = createPagesBrowserClient();
 
 export default function AccesoPage() {
   const user = useUser();
