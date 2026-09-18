9d07547ae28fdf3d8c56e918a85a564cda452051
Drworm82 <worm_jim@hotmail.com>
2026-09-18 13:37:59 -0600

    refactor: remove deprecated auth helper context

--- pages/_app.js
+++ pages/_app.js
@@ -1,22 +1,11 @@
-import { createBrowserClient } from '@supabase/ssr';
-import { SessionContextProvider } from '@supabase/auth-helpers-react';
 import { useState } from 'react';
 import Layout from '../components/Layout';
 import '../styles/globals.css';
 
 const App = ({ Component, pageProps }) => {
-  const [supabase] = useState(() =>
-    createBrowserClient(
-      process.env.NEXT_PUBLIC_SUPABASE_URL,
-      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
-    )
-  );
-
   return (
     <Layout>
-      <SessionContextProvider supabaseClient={supabase} initialSession={pageProps.initialSession}>
-        <Component {...pageProps} />
-      </SessionContextProvider>
+      <Component {...pageProps} />
     </Layout>
   );
 };
