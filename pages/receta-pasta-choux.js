fd947e58dcd4c8730dcafeb2546fdadf4d6f153b
Drworm82 <worm_jim@hotmail.com>
2026-09-22 16:34:54 -0600

    Corregir flujo de acceso en recetas protegidas

--- pages/receta-pasta-choux.js
+++ pages/receta-pasta-choux.js
@@ -1,4 +1,4 @@
-import { createCourseServerClient, requireCourseAccess } from '../lib/course';
+import { requireCourseAuth, requireCourseAccess } from '../lib/course';
 
 import CourseShell from '../components/course/CourseShell';
 
@@ -157,8 +157,7 @@ export default function RecipePastaChouxPage() {
 
 
 export async function getServerSideProps({ req, res }) {
-  const supabase = createCourseServerClient(req, res);
-  const { data: { claims } } = await supabase.auth.getClaims();
+  const { supabase, claims } = await requireCourseAuth(req, res);
 
   if (!claims) return { redirect: { destination: '/acceso', permanent: false } };
 
