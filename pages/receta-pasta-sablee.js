88dd7e085efaf5527cb329691f28ed525327f3d4
Drworm82 <worm_jim@hotmail.com>
2026-09-22 16:34:58 -0600

    Corregir flujo de acceso en recetas protegidas

--- pages/receta-pasta-sablee.js
+++ pages/receta-pasta-sablee.js
@@ -1,4 +1,4 @@
-import { createCourseServerClient, requireCourseAccess } from '../lib/course';
+import { requireCourseAuth, requireCourseAccess } from '../lib/course';
 
 import CourseShell from '../components/course/CourseShell';
 
@@ -44,8 +44,7 @@ export default function SableePage() {
 
 
 export async function getServerSideProps({ req, res }) {
-  const supabase = createCourseServerClient(req, res);
-  const { data: { claims } } = await supabase.auth.getClaims();
+  const { supabase, claims } = await requireCourseAuth(req, res);
 
   if (!claims) return { redirect: { destination: '/acceso', permanent: false } };
 
