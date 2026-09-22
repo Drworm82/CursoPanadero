9750daed2f7231ba4032944af39f6437d1469c28
Drworm82 <worm_jim@hotmail.com>
2026-09-22 14:46:02 -0600

    Proteger receta del curso tarta-de-frutas

--- pages/receta-tarta-de-frutas.js
+++ pages/receta-tarta-de-frutas.js
@@ -1,3 +1,5 @@
+import { createCourseServerClient, requireCourseAccess } from '../lib/course';
+
 import CourseShell from '../components/course/CourseShell';
 
 const ingredients = [
@@ -95,4 +97,16 @@ export default function FruitTartPage() {
       </div>
     </CourseShell>
   );
+}
+
+export async function getServerSideProps({ req, res }) {
+  const supabase = createCourseServerClient(req, res);
+  const { data: { claims } } = await supabase.auth.getClaims();
+
+  if (!claims) return { redirect: { destination: '/acceso', permanent: false } };
+
+  const hasAccess = await requireCourseAccess(supabase);
+  if (!hasAccess) return { notFound: true };
+
+  return { props: {} };
 }
