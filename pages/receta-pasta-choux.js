e8cbd3760c088595d5fc27b4ed8227648e91cc0c
Drworm82 <worm_jim@hotmail.com>
2026-09-22 14:45:33 -0600

    Proteger receta del curso pasta-choux

--- pages/receta-pasta-choux.js
+++ pages/receta-pasta-choux.js
@@ -1,3 +1,5 @@
+import { createCourseServerClient, requireCourseAccess } from '../lib/course';
+
 import CourseShell from '../components/course/CourseShell';
 
 const ingredients = [
@@ -151,4 +153,17 @@ export default function RecipePastaChouxPage() {
       </div>
     </CourseShell>
   );
+}
+
+
+export async function getServerSideProps({ req, res }) {
+  const supabase = createCourseServerClient(req, res);
+  const { data: { claims } } = await supabase.auth.getClaims();
+
+  if (!claims) return { redirect: { destination: '/acceso', permanent: false } };
+
+  const hasAccess = await requireCourseAccess(supabase);
+  if (!hasAccess) return { notFound: true };
+
+  return { props: {} };
 }
