647e0f3bbba0105339775b927a03fa83198d5d77
Drworm82 <worm_jim@hotmail.com>
2026-09-22 14:45:36 -0600

    Proteger receta del curso pasta-sablee

--- pages/receta-pasta-sablee.js
+++ pages/receta-pasta-sablee.js
@@ -1,3 +1,5 @@
+import { createCourseServerClient, requireCourseAccess } from '../lib/course';
+
 import CourseShell from '../components/course/CourseShell';
 
 const ingredients = [
@@ -38,4 +40,17 @@ export default function SableePage() {
       </div>
     </CourseShell>
   );
+}
+
+
+export async function getServerSideProps({ req, res }) {
+  const supabase = createCourseServerClient(req, res);
+  const { data: { claims } } = await supabase.auth.getClaims();
+
+  if (!claims) return { redirect: { destination: '/acceso', permanent: false } };
+
+  const hasAccess = await requireCourseAccess(supabase);
+  if (!hasAccess) return { notFound: true };
+
+  return { props: {} };
 }
