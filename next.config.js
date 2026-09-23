c344781d9d620ed3f6b4f2c9f4266d3e81cbfbcd
Drworm82 <worm_jim@hotmail.com>
2026-09-23 13:44:14 -0600

    feat(i18n): configure Spanish and English locales

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  i18n: {
+    locales: ['es', 'en'],
+    defaultLocale: 'es',
+    localeDetection: true,
+  },
   webpack: (config) => {
     config.resolve.fallback = {
       fs: false,
